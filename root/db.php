6a9aa074592468c823b30226496a3ec8e6e6a779
oc-rajeshu <rajesh.upadhayaya@creditsafe.co.in>
2020-04-01 19:13:14 +0530

    correcte env variable for heroku

--- root/db.php
+++ root/db.php
@@ -3,13 +3,13 @@
 $ON_HEROKU = getenv('ON_HEROKU');
 
 if($ON_HEROKU) {
-	$conn = mysqli_connect(getenv('HOST'), getenv('USERNAME'), getenv('PASSWORD')) or die(mysqli_error());
+	$conn = mysqli_connect(getenv('HOST'), getenv('USERNAME'), getenv('PASSWORD')) or die(mysqli_error($conn));
 	
 }else{
-	$conn = mysqli_connect('localhost', 'root', '') or die(mysqli_error());
+	$conn = mysqli_connect('localhost', 'root', '') or die(mysqli_error($conn));
 	
 }
 
 mysqli_set_charset($conn, 'utf8mb4');
-mysqli_select_db($conn, "crossword") or die(mysqli_error());	
+mysqli_select_db($conn, "crossword") or die(mysqli_error($conn));	
 ?>
