dc3f12fdec54ba0c024f5790b4c3d3c83a3249d4
oc-rajeshu <rajesh.upadhayaya@creditsafe.co.in>
2020-04-01 18:30:33 +0530

    updated db file

--- root/db.php
+++ root/db.php
@@ -1,16 +1,15 @@
 <?php
 
 $ON_HEROKU = getenv('ON_HEROKU');
-echo $ON_HEROKU;
-
-$conn = mysql_connect('localhost', 'root', '') or die(mysql_error());
-mysql_set_charset('utf8mb4',$conn);
-mysql_select_db("crossword") or die(mysql_error());	
-
 
 if($ON_HEROKU) {
 	$conn = mysql_connect(getenv('HOST'), getenv('USERNAME'), getenv('PASSWORD')) or die(mysql_error());
 	mysql_set_charset('utf8mb4',$conn);
 	mysql_select_db("crossword") or die(mysql_error());	
+}else{
+	$conn = mysql_connect('localhost', 'root', '') or die(mysql_error());
+	mysql_set_charset('utf8mb4',$conn);
+	mysql_select_db("crossword") or die(mysql_error());	
+
 }
 ?>
